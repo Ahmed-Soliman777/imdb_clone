3497674c7de9eb9cf935fe8f0211db6d210c2fd2
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-03-23 06:28:52 +0200

    Add alt img prop

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -11,7 +11,7 @@ export default async function MoviePage({ params }) {
     return (
         <div className='w-full'>
             <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
-                <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} width={500} height={300} className='rounded-lg' style={{ maxWidth: '100%', height: '100%' }} />
+                <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} alt='img' width={500} height={300} className='rounded-lg' style={{ maxWidth: '100%', height: '100%' }} />
                 <div className='p-2'>
                     <h2 className='text-lg mb-3 font-bold'>
                         {movie.title || movie.name}
